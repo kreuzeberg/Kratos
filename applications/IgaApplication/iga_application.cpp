74ff19dbb9abe291fa92abec9b4d82c2acec9735
Nicol <nantonelli@cimne.upc.edu>
2024-10-01 10:37:03 +0200

    creation of the nurbs_modeler_sbm (not optimized)

--- applications/IgaApplication/iga_application.cpp
+++ applications/IgaApplication/iga_application.cpp
@@ -90,6 +90,7 @@ KRATOS_INFO("") << "    KRATOS  _____ _____\n"
     KRATOS_REGISTER_MODELER("IgaModeler", mIgaModeler);
     KRATOS_REGISTER_MODELER("RefinementModeler", mRefinementModeler);
     KRATOS_REGISTER_MODELER("NurbsGeometryModeler", mNurbsGeometryModeler);
+    KRATOS_REGISTER_MODELER("NurbsGeometryModelerSBM", mNurbsGeometryModelerSBM);
 
     // VARIABLES
     KRATOS_REGISTER_VARIABLE(CROSS_AREA)
